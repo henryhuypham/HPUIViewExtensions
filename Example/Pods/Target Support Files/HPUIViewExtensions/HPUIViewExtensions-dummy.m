c0d66307fb4ec4358b74a4bdd4a5dc3eb1ccf560
Huy Pham <huyphamthanh8290@gmail.com>
2015-11-02 10:25:23 +0700

    Initial commit

---
+++ Example/Pods/Target Support Files/HPUIViewExtensions/HPUIViewExtensions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HPUIViewExtensions : NSObject
+@end
+@implementation PodsDummy_HPUIViewExtensions
+@end
---
+++ Example/Pods/Target Support Files/HPUIViewExtensions/HPUIViewExtensions-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double HPUIViewExtensionsVersionNumber;
+FOUNDATION_EXPORT const unsigned char HPUIViewExtensionsVersionString[];
+
