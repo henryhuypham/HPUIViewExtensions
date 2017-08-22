41d861d0808748430c5710dd2d328c38e765028b
Henry Pham <henry.huypham@gmail.com>
2017-08-22 19:06:35 +0700

    Add translation files

--- Example/HPUIViewExtensions/ViewController.swift
+++ Example/HPUIViewExtensions/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textField.setError(message: "Hahaha")
-        label.text = "Bla bla"
+//        textField.setError(message: "Hahaha")
+//        label.text = "Bla bla"
     }
 }
